3c5b2dba614d6c0173e0f91294454e63f9a1c5e5
tprhat <tomislav.prhat@gmail.com>
2024-12-06 13:47:10 +0100

    update template

--- template/main.go
+++ template/main.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println("Part 1:", part1("input.txt"))
+	t1 := time.Now()
+	fmt.Println("Part1 time: ", t1.Sub(start))
 	fmt.Println("Part 2:", part2("input.txt"))
+	t2 := time.Now()
+	fmt.Println("Part2 time: ", t2.Sub(t1))
 
 }
 
