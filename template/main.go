6cdc7c9b93ac0e23557b995831b39385471ca84b
tprhat <tomislav.prhat@gmail.com>
2024-12-10 17:43:23 +0100

    move main to the bottom

--- template/main.go
+++ template/main.go
@@ -7,16 +7,6 @@ import (
 	"time"
 )
 
-func main() {
-	start := time.Now()
-	fmt.Println("Part 1:", part1("input.txt"))
-	t1 := time.Now()
-	fmt.Println("Part1 time: ", t1.Sub(start))
-	fmt.Println("Part 2:", part2("input.txt"))
-	t2 := time.Now()
-	fmt.Println("Part2 time: ", t2.Sub(t1))
-}
-
 func part1(name string) int {
 	return 0
 }
@@ -36,4 +26,14 @@ func transformInput(name string) []string {
 		lines = append(lines, scanner.Text())
 	}
 	return lines
+}
+
+func main() {
+	start := time.Now()
+	fmt.Println("Part 1:", part1("input.txt"))
+	t1 := time.Now()
+	fmt.Println("Part1 time: ", t1.Sub(start))
+	fmt.Println("Part 2:", part2("input.txt"))
+	t2 := time.Now()
+	fmt.Println("Part2 time: ", t2.Sub(t1))
 }
